4f215a1e01612629a92ffc8d51157f42565d9d09
Runke Tang <u202210127@hust.edu.cn>
2026-03-27 11:08:53 +0800

    Add recollimated_shock.cpp with necessary headers

---
+++ src/pgen/recollimated_shock.cpp
@@ -0,0 +1,15 @@
+// C++ headers
+#include <cmath>      // sqrt()
+
+// Athena++ headers
+#include "../athena.hpp"
+#include "../athena_arrays.hpp"
+#include "../bvals/bvals.hpp"
+#include "../coordinates/coordinates.hpp"
+#include "../eos/eos.hpp"
+#include "../field/field.hpp"
+#include "../hydro/hydro.hpp"
+#include "../mesh/mesh.hpp"
+#include "../parameter_input.hpp"
+#include "../units/units.hpp"
+
