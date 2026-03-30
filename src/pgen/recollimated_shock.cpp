087d2b2d67af7febb96c91f548d511a63db4ec0b
Runke Tang <u202210127@hust.edu.cn>
2026-03-30 12:28:56 +0800

    Add new variables and refactor calculations in recollimated_shock

--- src/pgen/recollimated_shock.cpp
+++ src/pgen/recollimated_shock.cpp
@@ -16,8 +16,13 @@
 namespace {
 Real rho_amb, T_amb, bz_amb;
 Real power_jet, v_jet, bz_jet;
-Real theta;
+Real power_iso, v_iso, r_source, theta , theta_rad;
 Real gm1;
+// 预计算的转换值 (Code Units)
+Real d_amb, p_amb, bz_amb_ism, bz_jet_ism;
+Real power_iso_code, power_jet_code;
+Real M_dot_jet, M_dot_iso;
+Real code_mass_cgs, code_time_cgs, code_velocity_cgs, code_energydensity_cgs, code_magneticfield_cgs;
 } // namespace
 
 void Source(MeshBlock *pmb, const Real time, const Real dt,
@@ -26,6 +31,12 @@ void Source(MeshBlock *pmb, const Real time, const Real dt,
               AthenaArray<Real> &cons_scalar);
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
+  Units units(pin); 
+  code_mass_cgs = units.code_mass_cgs;
+  code_time_cgs = units.code_time_cgs;
+  code_velocity_cgs  = units.code_velocity_cgs;
+  code_energydensity_cgs = units.code_energydensity_cgs;
+  code_magneticfield_cgs = units.code_magneticfield_cgs;
   // initialize global variables
   rho_amb  = pin->GetReal("problem", "rho_amb");//number density of ISM in 1/cm^3
   T_amb  = pin->GetReal("problem", "T_amb");//temperature of ISM in K
@@ -41,7 +52,15 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
   power_iso = pin->GetReal("problem","power_iso");//wind power in erg/s
   v_iso = pin->GetReal("problem", "v_iso");//speed of wind in km/s
   theta=pin->GetReal("problem","theta");//half opening angle in degree
+  
+  theta_rad=theta*PI/180.0;
+  power_iso_code=power_iso*code_time_cgs/(code_mass_cgs*code_velocity_cgs*code_velocity_cgs);
+  M_dot_iso=2.0*power_iso_code/SQR(v_iso);//mass lossing rate in code unit
+  power_jet_code=power_jet*code_time_cgs/(code_mass_cgs*code_velocity_cgs*code_velocity_cgs);
+  M_dot_jet=2.0*power_jet_code/SQR(v_jet);//mass lossing rate in code unit
+  
   EnrollUserExplicitSourceFunction(Source);
+  
   return;
 }
 
@@ -94,8 +113,8 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
     for (int k = ks; k <= ke+1; ++k) {
       for (int j = js; j <= je; ++j) {
         for (int i = is; i <= ie; ++i) {
-          Real R = pmb->pcoord->x1v(i);
-          Real z = pmb->pcoord->x3v(k);
+          Real R = pcoord->x1v(i);
+          Real z = pcoord->x3v(k);
           Real r = std::sqrt(SQR(R)+SQR(z));
           Real mu = z/r;
           if (r < 1.0*r_source && r> 0.25*r_source) {
@@ -140,32 +159,30 @@ void Source(MeshBlock *pmb, const Real time, const Real dt,
         Real r = std::sqrt(SQR(R)+SQR(z));
         Real mu = z/r;
         if (r < 1.0*r_source && r> 0.25*r_source) {
-           Real angle=std::acos(z/r);
-           Real theta_rad=theta*PI/180.0;
+           Real cos_val = std::fmin(1.0, std::fmax(-1.0, z/r));
+           Real angle = std::acos(cos_val);
+           
            Real ftheta_rad=1.0/(1.0+std::exp(10.0*(angle-theta_rad)/theta_rad))
              +1.0/(1.0+std::exp(10.0*(PI-angle-theta_rad)/theta_rad));
            Real btemp=0.45*r_source;
            Real fr=1.0/(1.0+std::exp(-40.0*(r-btemp)/btemp))
              *1.0/(1.0+std::exp(40.0*(r-2.0*btemp)/btemp));
            
-           Real power_iso_code=power_iso*code_time_cgs/(code_mass_cgs*code_velocity_cgs*code_velocity_cgs);
-           Real M_dot_iso=2.0*power_iso_code/SQR(v_iso);//mass lossing rate in code unit
            Real d_iso=M_dot_iso/(4*PI*r*r*v_iso);
-           Real power_jet_code=power_jet*code_time_cgs/(code_mass_cgs*code_velocity_cgs*code_velocity_cgs);
-           Real M_dot_jet=2.0*power_jet_code/SQR(v_jet);//mass lossing rate in code unit
+           
            Real d_jet=M_dot_jet/(2*PI*r*r*(1-std::cos(theta_rad))*v_jet);
 
-           dens_current=cons(IDN,k,j,i);
-           p1_current=cons(IM1,k,j,i);
-           p2_current=cons(IM2,k,j,i);
-           p3_current=cons(IM3,k,j,i);
-           energy_current=cons(IEN,k,j,i);
+           Real dens_current=cons(IDN,k,j,i);
+           Real p1_current=cons(IM1,k,j,i);
+           Real p2_current=cons(IM2,k,j,i);
+           Real p3_current=cons(IM3,k,j,i);
+           Real energy_current=cons(IEN,k,j,i);
 
-           dens_target=d_jet*ftheta_rad+d_iso*(1-ftheta_rad);
-           p1_target=d_jet*v_jet*R/r*ftheta_rad+d_iso*v_iso*R/r*(1-ftheta_rad);
-           p2_target=0.0;
-           p3_target=d_jet*v_jet*z/r*ftheta_rad+d_iso*v_iso*z/r*(1-ftheta_rad);
-           energy_target=0.5*d_jet*v_jet*v_jet*ftheta_rad+0.5*d_iso*v_iso*v_iso*(1-ftheta_rad)
+           Real dens_target=d_jet*ftheta_rad+d_iso*(1-ftheta_rad);
+           Real p1_target=d_jet*v_jet*R/r*ftheta_rad+d_iso*v_iso*R/r*(1-ftheta_rad);
+           Real p2_target=0.0;
+           Real p3_target=d_jet*v_jet*z/r*ftheta_rad+d_iso*v_iso*z/r*(1-ftheta_rad);
+           Real energy_target=0.5*d_jet*v_jet*v_jet*ftheta_rad+0.5*d_iso*v_iso*v_iso*(1-ftheta_rad)
              +0.5*(bcc(IB1,k,j,i)*bcc(IB1,k,j,i)+bcc(IB2,k,j,i)*bcc(IB2,k,j,i)+bcc(IB3,k,j,i)*bcc(IB3,k,j,i))
              +p_amb/(g-1.0);
 
